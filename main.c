c8e9c808c727a82993db32fc0d050b853e7f68df
aiyinsichen <aiyinsichen@qq.com>
2019-01-18 10:13:03 +0800

    构建成绩查询系统基本框架

--- main.c
+++ main.c
@@ -1,8 +1,59 @@
 #include <stdio.h>
+struct student
+{
+    int yuwen;
+    int shuxue;
+    int yingwu;
+    char name[100];
+};
+
 
 int main(){
 
-    printf("hello world!");
+    printf("xueshengchengjiguanluxitong\n");
+
+
+    while(1){
+        printf("1--tianjiaxueshengchengji\n");
+        printf("2--shanchuxueshengchengji\n");
+        printf("3--xianshisuoyouxueshengchengji\n");
+        printf("4--yewu1:suoyouyzongfenbujigedexuesheng(xiaoyu180)\n");
+        printf("5--yewu2:meiyikedoubujigedexuesheng\n");
+        printf("6--yewu3:xianshizongfenzuigaodexuesheng\n");
+        printf("7--yewu4:xianshipingjunfenzuigao,qiemeiyoubujigekemudexuesheng\n");
+        printf("8--tuichu\n");
+
+
+        printf("qingxuanze:\n");
+
+        int code;
+        scanf("%d",&code);
+        
+        if(code == 1){
+           
+        }
+        if(code == 2){
+            
+        }
+        if(code == 3){
+            
+        }
+        if(code == 4){
+            
+        }
+        if(code == 5){
+            
+        }
+        if(code == 6){
+            
+        }
+        if(code == 7){
+            
+        }
+        if(code == 8){
+            
+        }
+    }
 
     return 0;
 
